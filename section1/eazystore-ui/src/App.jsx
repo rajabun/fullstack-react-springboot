c42d0e123e17753e66b98e8bf794bef6979ef6e7
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-18 06:20:24 +0700

    some training after read pdf

--- section1/eazystore-ui/src/App.jsx
+++ section1/eazystore-ui/src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const logoStyle = {
+    height: '6em',
+    padding: '1.5em',
+    transition: 'filter 300ms',
+  }
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img style = {logoStyle} src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img style = {logoStyle} src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className= {`${styles.card}`}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
--- section1/eazystore/src/main/java/com/eazybites/eazystore/controller/HelloController.java
+++ section1/eazystore/src/main/java/com/eazybites/eazystore/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.eazybites.eazystore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
 }
