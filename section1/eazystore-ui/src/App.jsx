22f87c4e5bd8544ed96d94cef9686cb05cd87f2a
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-21 14:38:48 +0700

    finish section 2

--- section1/eazystore-ui/src/App.jsx
+++ section1/eazystore-ui/src/App.jsx
@@ -1,28 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import styles from './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import styles from './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const logoStyle = {
-    height: '6em',
-    padding: '1.5em',
-    transition: 'filter 300ms',
-  }
+    height: "6em",
+    padding: "1.5em",
+    transition: "filter 300ms",
+  };
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img style = {logoStyle} src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            style={logoStyle}
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img style = {logoStyle} src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            style={logoStyle}
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className= {`${styles.card}`}>
+      {/* <div className= {`${styles.card}`}> */}
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -34,7 +46,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- section1/eazystore-ui/vite.config.js
+++ section1/eazystore-ui/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  /* to configure build server to custom port. default port from vite is 5173
+  server: {
+    port: 5000,
+  },
+  */
+});
