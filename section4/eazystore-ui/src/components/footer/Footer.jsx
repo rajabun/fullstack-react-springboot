22b157ec31fae3a90cfda753c5b734aa9ca6218b
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-22 13:51:50 +0700

    section 4 chapter 53 css modules

--- section4/eazystore-ui/src/components/footer/Footer.jsx
+++ section4/eazystore-ui/src/components/footer/Footer.jsx
@@ -1,26 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import "./footer.css";
+// import "./footer.css";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   const isActive = Math.random() > 0.5;
 
   return (
     <>
-      <h1
+      {/* <h1
         style={{
           textAlign: "center",
           color: isActive ? "#5b21b6" : "#333",
         }}
       >
         Demo of Global CSS Scope from Footer
-      </h1>
+      </h1> */}
       {/* <h1 className="my-heading">Demo of Global CSS Scope from Footer</h1> */}
-      <footer className="footer">
+
+      <h1
+        className={`${styles["my-heading"]} ${
+          isActive ? styles["primary-color"] : styles["secondary-color"]
+        }`}
+      >
+        Demo of Global CSS Scope from Footer
+      </h1>
+      <footer className={styles.footer}>
         Built with
         <FontAwesomeIcon
           icon={faHeart}
-          className="footer-icon"
+          className={styles["footer-icon"]}
           aria-hidden="true"
         />
         by
