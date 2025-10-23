ff7e59bbabfc7413efb4aec3247140623cc42936
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-23 09:58:45 +0700

    finish section 4

--- section4/eazystore-ui/src/App.jsx
+++ section4/eazystore-ui/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./custom.scss";
 
 function App() {
   return (
