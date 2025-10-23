86efbc38c7f3f0dba7c097cad74dd5cd48be66b5
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-23 09:43:00 +0700

    section 4 chapter 61 styling form

--- section4/eazystore-ui/src/components/Home.jsx
+++ section4/eazystore-ui/src/components/Home.jsx
@@ -103,6 +103,48 @@ export default function Home() {
           <div className="col-3 border p-3 bg-danger text-white">Column 3</div>
         </div>
 
+        <div className="container col-8 justify-content-center mt-3 border border-dark rounded">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div class="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+
         <PageHeading title="Explore Eazy Stickers!">
           Add a touch of creativity to your space with our wide range of fun and
           unique stickers. Perfect for any occasion!
