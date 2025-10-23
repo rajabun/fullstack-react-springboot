131db3133883bdc850798fdd1e88da18ae8d00ce
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-23 09:26:02 +0700

    section 4 chapter 59 styling alerts

--- section4/eazystore-ui/src/App.jsx
+++ section4/eazystore-ui/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/footer/Footer";
 import Home from "./components/Home";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- section4/eazystore-ui/src/components/Home.jsx
+++ section4/eazystore-ui/src/components/Home.jsx
@@ -34,6 +34,36 @@ export default function Home() {
           <BootstrapButton text="Delete" type="warning" />
           <BootstrapButton text="Link" type="link" />
         </div>
+        <div className="d-grid gap-2 col-8 mx-auto">
+          <div class="alert alert-primary text-center" role="alert">
+            A simple primary alert—check it out!
+          </div>
+          <div className="alert alert-secondary text-center" role="alert">
+            A simple secondary alert—check it out!
+          </div>
+          <div className="alert alert-success text-center" role="alert">
+            A simple success alert—check it out!
+          </div>
+          <div className="alert alert-danger text-center" role="alert">
+            A simple danger alert—check it out!
+          </div>
+          <div className="alert alert-warning text-center" role="alert">
+            A simple warning alert—check it out!
+          </div>
+
+          <div
+            className="alert alert-warning alert-dismissible fade show text-center"
+            role="alert"
+          >
+            <strong>Warning!</strong> This is a warning alert!
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
         <PageHeading title="Explore Eazy Stickers!">
           Add a touch of creativity to your space with our wide range of fun and
           unique stickers. Perfect for any occasion!
