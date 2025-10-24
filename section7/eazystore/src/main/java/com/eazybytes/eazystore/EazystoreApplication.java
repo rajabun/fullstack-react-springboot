18f4b76f8f50238bd4625d110f107750040011ab
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-24 16:13:13 +0700

    section 7 first REST API demos chapter 110-112

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -3,9 +3,17 @@ package com.eazybytes.eazystore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.eazybytes.eazystore.controller"}) //optional
 public class EazystoreApplication {
+
+    /*
+    When you are defining all your java classes, which are supposed to be created as the beans
+    by the spring framework under the same package where your Spring Boot main class is present,
+    then there is no need of mentioning @ComponentScan on top of your Spring Boot main class
+     */
 
 	public static void main(String[] args) {
 		SpringApplication.run(EazystoreApplication.class, args);
