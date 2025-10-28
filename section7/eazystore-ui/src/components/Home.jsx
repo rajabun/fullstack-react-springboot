2575045be8962a4d4e053856e114bfc45b61436d
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-28 15:22:19 +0700

    section 7 chapter 127 adding some changes in ui to invoke backend API

--- section7/eazystore-ui/src/components/Home.jsx
+++ section7/eazystore-ui/src/components/Home.jsx
@@ -1,9 +1,23 @@
-import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import apiClient from "../api/apiClient";
+import { useState, useEffect } from "react";
 
+//Hooks
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  //Run once when the component mounts
+  //Mounting is the process of creating and adding the component into DOM
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get("products"); //Axios GET Request
+    setProducts(response.data); //Update products state with fetched data
+  };
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
