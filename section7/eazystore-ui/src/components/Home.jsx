80599108ef074bedfe6f4a1b7aa26eaf9f847301
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-28 14:44:12 +0700

    section 7 chapter 126 installing axios library in ui app for api calls

---
+++ section7/eazystore-ui/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import products from "../data/products";
+import PageHeading from "./PageHeading";
+import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient";
+
+export default function Home() {
+  return (
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
+      <PageHeading title="Explore Eazy Stickers!">
+        Add a touch of creativity to your space with our wide range of fun and
+        unique stickers. Perfect for any occasion!
+      </PageHeading>
+      <ProductListings products={products} />
+    </div>
+  );
+}
