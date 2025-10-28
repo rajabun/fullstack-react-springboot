b0b97b263e8b67011f828c78644e30b541a37a9f
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-28 16:12:24 +0700

    section 7 chapter 130 showing loading & error message in home page ui

--- section7/eazystore-ui/src/components/Home.jsx
+++ section7/eazystore-ui/src/components/Home.jsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 //Hooks
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   //Run once when the component mounts
   //Mounting is the process of creating and adding the component into DOM
@@ -14,9 +16,35 @@ export default function Home() {
   }, []);
 
   const fetchProducts = async () => {
-    const response = await apiClient.get("products"); //Axios GET Request
-    setProducts(response.data); //Update products state with fetched data
+    try {
+      setLoading(true);
+      const response = await apiClient.get("products"); //Axios GET Request
+      setProducts(response.data); //Update products state with fetched data
+    } catch (error) {
+      setError(
+        error.response?.data?.message ||
+          "Failed to fetch products. Please try again."
+      ); //Extract error messages if available
+    } finally {
+      setLoading(false);
+    }
   };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl font-semibold">Loading products...</span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl text-red-500">Error: {error}</span>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
--- section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,10 +2,18 @@ package com.eazybytes.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+/* optional annotations, use this if repository & entity is in different package
+@EnableJpaRepositories
+@EntityScan
+*/
+
 //@ComponentScan(basePackages = {"com.eazybytes.eazystore.controller"}) //optional
 public class EazystoreApplication {
 
