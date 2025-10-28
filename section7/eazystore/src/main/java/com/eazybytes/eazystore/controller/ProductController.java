18259aa9bace02df36d5b2333bc3fb55b1b9bc31
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-28 15:59:33 +0700

    section 7 chapter 128-129 part 2

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazystore.controller;
 import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
+//@CrossOrigin(origins = "http://localhost:5173") 1st way to fix cors error
 @RequiredArgsConstructor
 public class ProductController {
 
