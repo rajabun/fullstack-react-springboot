4f44b63b1f0350f8c48044b08840f531d0a1ca32
Muhammad Rajab <ichijo_rajab@hotmail.com>
2025-10-29 09:58:48 +0700

    section 10 chapter 149-150 introduction & defining react routes

---
+++ section9/eazystore-ui/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./footer/Footer";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <div>About</div>
+      <Footer />
+    </>
+  );
+}
