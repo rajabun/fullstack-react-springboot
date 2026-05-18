2c4ad55f60a9ef87559782a55d6a3becff3b9ec9
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-05-18 15:06:42 +0700

    section 10 chapter 154 efficient router with createRoutesFromElements

--- section_10/eazystore-ui/src/main.jsx
+++ section_10/eazystore-ui/src/main.jsx
@@ -3,13 +3,33 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Home from "./components/Home.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Login from "./components/Login.jsx";
 import Cart from "./components/Cart.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 
+//efficient writing for router
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>,
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
+
+/*
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -43,6 +63,7 @@ const appRouter = createBrowserRouter([
     ],
   },
 ]);
+*/
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
