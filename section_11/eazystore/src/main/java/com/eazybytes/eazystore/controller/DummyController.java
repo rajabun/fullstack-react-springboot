923e7c0be90c4e3f08850addc4eccf482467825e
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-05-22 09:59:25 +0700

    section 11 chapter 174 Mapping HTTP Query Parameters Using @RequestParam

--- section_11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
+++ section_11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
@@ -1,8 +1,12 @@
 package com.eazybytes.eazystore.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.eazystore.dto.UserDto;
@@ -15,6 +19,33 @@ public class DummyController {
     public String createUser(@RequestBody UserDto userDto) {
         System.out.print(userDto);
         return "User created successfully";
+    }
+
+    @GetMapping("/search")
+    public String searchUser(
+        @RequestParam(
+        required = false,
+        defaultValue = "Guest",
+        name = "name")
+        String userName) {
+            System.out.print(userName);
+            return "Searching for user: " + userName;
+    }
+
+    @GetMapping("/multiple-search")
+    public String multipleSearch(
+        @RequestParam String firstName,
+        @RequestParam String lastName) {
+            String actualName = firstName + " " + lastName;
+            System.out.print(actualName);
+            return "Searching for user: " + actualName;
+    }
+
+    @GetMapping("/multiple-search-map")
+    public String multipleSearchWithMap(@RequestParam Map<String,String> params) {
+            String actualName = params.get("firstName") + " " + params.get("lastName") + " Map";
+            System.out.print(actualName);
+            return "Searching for user: " + actualName;
     }
 
 }
