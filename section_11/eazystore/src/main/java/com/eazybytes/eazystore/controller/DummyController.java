a81edf84c4e3a7f6068a9ab1139fc32d4b9065a9
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-05-22 10:16:29 +0700

    section 11 chapter 175 Extracting Path Parameters using @PathVariable

--- section_11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
+++ section_11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazystore.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,30 @@ public class DummyController {
             String actualName = params.get("firstName") + " " + params.get("lastName") + " Map";
             System.out.print(actualName);
             return "Searching for user: " + actualName;
+    }
+
+    /*
+    @GetMapping("/user/{userId}")
+    public String getUser(@PathVariable(name = "userId") String id) {
+            System.out.print(id);
+            return "Searching for user: " + id;
+    }
+    */
+
+    @GetMapping({"/user/{userId}/posts/{postId}", "/user/{userId}"})
+    public String getUserMultiplePath(
+        @PathVariable(name = "userId") String id,
+        @PathVariable(required = false) String postId) {
+            String actualId = id + " and post: " + postId;
+            System.out.print(actualId);
+            return "Searching for user: " + actualId;
+    }
+
+    @GetMapping({"/user/map/{userId}/posts/{postId}", "/user/map/{userId}"})
+    public String getUserWithMap(@PathVariable Map<String,String> pathVariables) {
+            String actualId = pathVariables.get("userId") + " and post: " + pathVariables.get("postId");
+            System.out.print(actualId);
+            return "Searching for user: " + actualId;
     }
 
 }
