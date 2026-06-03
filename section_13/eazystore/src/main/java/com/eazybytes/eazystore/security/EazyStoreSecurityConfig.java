ae8ed32c31adbf7dcb41bedb74ed08c767e6b4f2
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-06-03 13:03:46 +0700

    section 13 chapter 217-218 Creating users using InMemoryUserDetailsManager

--- section_13/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ section_13/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -52,6 +57,30 @@ public class EazyStoreSecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
+    }
+
+    //for development purpose. this method is not recommended for production use
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user1 = User.builder()
+        .username("rajabun")
+        // .password("eazystore") //real password, use https://bcrypt-generator.com to generate hash
+        .password("$2a$12$IK7CFW.Es1pLWgUgcjNK6.a7MccA/YDqgsR.tQFK1VnBHzdSnpaY.")
+        .roles("USER").build();
+
+        var user2 = User.builder()
+        .username("admin")
+        // .password("admin") //real password, use https://bcrypt-generator.com to generate hash
+        .password("$2a$12$YM.7H0vDb4FuvfO85/bGC.8tB0bO4/YGxUGlw6NHwI.vF3vzxTY5u")
+        .roles("USER", "ADMIN").build();
+
+        var memoryUserDetailManager = new InMemoryUserDetailsManager(user1, user2);
+        return memoryUserDetailManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
