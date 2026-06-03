838b37fd8e45c826be667a3da0368ca6f02183a0
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-06-03 15:20:39 +0700

    section 13 chapter 225-226 Generating JWT token in backend app

--- section_13/eazystore/src/main/java/com/eazybytes/eazystore/controller/AuthController.java
+++ section_13/eazystore/src/main/java/com/eazybytes/eazystore/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.eazystore.dto.LoginRequestDto;
 import com.eazybytes.eazystore.dto.LoginResponseDto;
+import com.eazybytes.eazystore.dto.UserDto;
+import com.eazybytes.eazystore.util.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
@@ -30,11 +34,18 @@ public class AuthController {
             var userNamePasswordAuth = new UsernamePasswordAuthenticationToken(
             loginRequestDto.username(),
             loginRequestDto.password());
-            System.out.println(loginRequestDto);
+
             Authentication authentication = authenticationManager.authenticate(userNamePasswordAuth);
+
+            var userDto = new UserDto();
+            var loggedInUser = (User) authentication.getPrincipal();
+            userDto.setName(loggedInUser.getUsername());
+
+            String jwtToken = jwtUtil.generateJwtToken(authentication);
+
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new LoginResponseDto(HttpStatus.OK.getReasonPhrase(),
-                null, null));
+                userDto, jwtToken));
         } catch (BadCredentialsException ex) {
             return buildErrorResponse(HttpStatus.UNAUTHORIZED,
                 "Invalid username or password");
