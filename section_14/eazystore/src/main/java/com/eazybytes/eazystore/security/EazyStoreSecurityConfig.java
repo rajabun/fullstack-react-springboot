21da094b3d8d4e7bae3d23f2f7bb8cbf9ee235c3
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-07-06 15:14:37 +0700

    section 14 chapter 237 Creating customers table to store end user details

--- section_14/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ section_14/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -61,26 +61,7 @@ public class EazyStoreSecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
-
-    //for development purpose. this method is not recommended for production use
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var user1 = User.builder()
-        .username("rajabun")
-        // .password("eazystore") //real password, use https://bcrypt-generator.com to generate hash
-        .password("$2a$12$IK7CFW.Es1pLWgUgcjNK6.a7MccA/YDqgsR.tQFK1VnBHzdSnpaY.")
-        .roles("USER").build();
-
-        var user2 = User.builder()
-        .username("admin")
-        // .password("admin") //real password, use https://bcrypt-generator.com to generate hash
-        .password("$2a$12$YM.7H0vDb4FuvfO85/bGC.8tB0bO4/YGxUGlw6NHwI.vF3vzxTY5u")
-        .roles("USER", "ADMIN").build();
-
-        var memoryUserDetailManager = new InMemoryUserDetailsManager(user1, user2);
-        return memoryUserDetailManager;
-    }
-
+    
     @Bean
     public AuthenticationManager authenticationManager(
         UserDetailsService userDetailsService,
