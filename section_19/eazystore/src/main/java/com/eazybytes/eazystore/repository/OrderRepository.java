c0c1823ae532850ba4641287fa988666230b8d51
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-07-15 21:12:49 +0700

    section 19 chapter 294 Writing custom queries using @Query with inline JPQL (Most Common Way)

--- section_19/eazystore/src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
+++ section_19/eazystore/src/main/java/com/eazybytes/eazystore/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package com.eazybytes.eazystore.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eazybytes.eazystore.entity.Customer;
 import com.eazybytes.eazystore.entity.Order;
@@ -15,4 +17,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
   List<Order> findByCustomerOrderByCreatedAtDesc(Customer customer);
 
   List<Order> findByOrderStatus(String orderStatus);
+
+  @Query("SELECT o FROM Order o WHERE o.customer = :customer ORDER BY o.createdAt DESC")
+  List<Order> findOrdersByCustomer(@Param("customer") Customer customer);
+
+  @Query("SELECT o FROM Order o WHERE o.orderStatus =?1")
+  List<Order> findOrdersByStatus(String orderStatus);
 }
--- section_19/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/OrderServiceImpl.java
+++ section_19/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/OrderServiceImpl.java
@@ -57,13 +57,13 @@ public class OrderServiceImpl implements IOrderService {
     @Override
     public List<OrderResponseDto> getCustomerOrders() {
         Customer customer =profileService.getAuthenticatedCustomer();
-        List<Order> orders = orderRepository.findByCustomerOrderByCreatedAtDesc(customer);
+        List<Order> orders = orderRepository.findOrdersByCustomer(customer);
         return orders.stream().map(this::mapToOrderResponseDTO).collect(Collectors.toList());
     }
 
     @Override
     public List<OrderResponseDto> getAllPendingOrders() {
-        List<Order> orders = orderRepository.findByOrderStatus(ApplicationConstants.ORDER_STATUS_CREATED);
+        List<Order> orders = orderRepository.findOrdersByStatus(ApplicationConstants.ORDER_STATUS_CREATED);
         return orders.stream().map(this::mapToOrderResponseDTO).collect(Collectors.toList());
     }
 
