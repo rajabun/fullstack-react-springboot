105c17dee956ed35def8981d59e46d5bd999cd42
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-07-15 10:54:42 +0700

    start section 17

---
+++ section_17/eazystore/src/main/java/com/eazybytes/eazystore/util/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.eazybytes.eazystore.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import com.eazybytes.eazystore.entity.Customer;
+import javax.crypto.SecretKey;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import com.eazybytes.eazystore.constants.ApplicationConstants;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+    private final Environment env;
+
+    public String generateJwtToken(Authentication authentication) {
+        String jwt = "";
+        String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        Customer fetchedCustomer = (Customer) authentication.getPrincipal();
+        String roles = authentication
+                .getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+        jwt = Jwts.builder().issuer("Eazy Store").subject("JWT Token")
+                .claim("username", fetchedCustomer.getName())
+                .claim("email", fetchedCustomer.getEmail())
+                .claim("mobileNumber", fetchedCustomer.getMobileNumber())
+                .claim("roles", roles)
+                .issuedAt(new java.util.Date())
+                .expiration(new java.util.Date((new java.util.Date()).getTime() + 60 * 60 * 1000))
+                .signWith(secretKey).compact();
+        return jwt;
+    }
+}
