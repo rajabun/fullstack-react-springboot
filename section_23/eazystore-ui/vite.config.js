85cdfc73f13dca4f0c4532dae86f349a43da4ae8
Muhammad Rajab <ichijo_rajab@hotmail.com>
2026-07-17 10:26:09 +0700

    section 23 chapter 333 Preparin React UI app for production deployment

--- section_23/eazystore-ui/vite.config.js
+++ section_23/eazystore-ui/vite.config.js
@@ -5,6 +5,27 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+          router: ["react-router-dom"],
+          ui: [
+            "@fortawesome/react-fontawesome",
+            "@fortawesome/fontawesome-svg-core",
+          ],
+        },
+      },
+    },
+  },
+  base: "/",
+  server: { port: 5173 },
+  preview: { port: 5173 },
 
   /* to configure build server to custom port. default port from vite is 5173
   server: {
